3f18b47c7299d734c81294f2cbbe367b064b011b
unblockzone <bjhuckle@students.wcpss.net>
2025-05-18 10:23:09 -0400

    bro kms

---
+++ tung-tung-sahur-horror/lib/runtimeData.js
@@ -0,0 +1,70 @@
+const runtimeData = (function () {
+
+    return {
+
+        // Basic information.
+        companyName: "DarkGame",
+        productName: "Tung Tung Tung Sahur Story",
+        productVersion: "1.9",
+        sdkVersion: "3.19.12+merge4",
+        productDescription: "",
+
+        // File references.
+        buildURL: "bin",
+        loaderURL: "bin/Tung tung tung sahur_Web_YandexGames.loader.js",
+        dataURL: "bin/Tung tung tung sahur_Web_YandexGames.data.gz",
+        frameworkURL: "bin/Tung tung tung sahur_Web_YandexGames.framework.js.gz",
+        workerURL: "",
+        codeURL: "bin/Tung tung tung sahur_Web_YandexGames.wasm.gz",
+        symbolsURL: "",
+        streamingURL: "streaming",
+
+        // Visual information.
+        logoType: "LOGO_TYPE",
+        iconTextureName: "ChatGPT Image 22 апр. 2025 г., 17_18_16.png",
+        backgroundTextureName: "background_1280x720.png",
+
+        // Aspect ratio.
+        desktopAspectRatio: -1,
+        mobileAspectRatio: -1,
+
+        // Debug mode.
+        debugMode: false,
+        rotationLockType : "None",
+
+        // Prefs.
+        prefsContainerTags: [ "json-data" ],
+
+        // Platform specific scripts.
+        wrapperScript: "yandexGamesWrapper.js",
+
+        // YandexGames.
+        yandexGamesSDK: "/sdk.js",
+
+        // Yandex Ads Network.
+        yandexGameId: "",
+        yandexBannerId: "",
+        yandexInterstitialDesktopId: "",
+        yandexInterstitialMobileId: "",
+        yandexRewardedDesktopId: "",
+        yandexRewardedMobileId: "",
+
+        // GameDistribution.
+        gameDistributionId: "",
+        gameDistributionPrefix: "mirragames_",
+
+        // CrazyGames.
+        crazyGamesXSollaProjectId: "",
+
+        // Ads by Google.
+        googleAdsClient: "",
+        googleAdsChannel: "",
+        googleAdsTest: true,
+
+        // GamePush.
+        gamepushProjectId: "",
+        gamepushToken: "",
+
+    }
+
+})();
